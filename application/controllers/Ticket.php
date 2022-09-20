042b38f97e05258fa6bd796ec4da72d4e4d1935f
bambenxx-studio <bambenxx@gmail.com>
2022-09-20 15:17:38 +0700

    update

--- application/controllers/Ticket.php
+++ application/controllers/Ticket.php
@@ -181,33 +181,6 @@ class Ticket extends CI_Controller
 
     private function kirimWablas($phone, $message)
     {
-        // $link  =  "https://app.ruangwa.id/api/send_message";
-        // $data = [
-        //     'phone' => $phone,
-        //     'message' => $msg,
-        // ];
-
-
-        // $curl = curl_init();
-        // $token =  "WDK2n8KAVuNw7r21pmrZCVqyfpDXUHypHCPcChUvCa1a15y2vt";
-
-        // curl_setopt(
-        //     $curl,
-        //     CURLOPT_HTTPHEADER,
-        //     array(
-        //         "Authorization: $token",
-        //     )
-        // );
-        // curl_setopt($curl, CURLOPT_URL, $link);
-        // curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        // curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-        // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
-        // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-        // $result = curl_exec($curl);
-        // curl_close($curl);
-        // return $result;
-
         $curl = curl_init();
         $token = "fY3R48uL8XTJrHIxZLej65cnynr0ZNsBEN2GOH5Ghcra1A2mjQOvbkohDkduJYyi";
         $link  =  "https://jogja.wablas.com/api/send-message";
